072f8fede73d6e42bd4df45ddf060695c2f92bdd
echosimanjuntak <echo.w.m.simanjuntak@gvmnetworks.com>
2019-09-27 22:23:22 +0700

    Implement Interfaces for Services

---
+++ pet-clinic-data/src/main/java/com/echo/petclinic/services/OwnerService.java
@@ -0,0 +1,14 @@
+package com.echo.petclinic.services;
+
+import com.echo.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+
+}
---
+++ pet-clinic-data/src/main/java/com/echo/petclinic/services/PetService.java
@@ -0,0 +1,13 @@
+package com.echo.petclinic.services;
+
+import com.echo.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+
+}
