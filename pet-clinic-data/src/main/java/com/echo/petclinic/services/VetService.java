91ebff6ed23bec9668bfb16fa72decd8e380e637
echosimanjuntak <echo.w.m.simanjuntak@gvmnetworks.com>
2019-09-28 10:52:05 +0700

    refactoring to common crud interface

--- pet-clinic-data/src/main/java/com/echo/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/echo/petclinic/services/VetService.java
@@ -2,12 +2,6 @@ package com.echo.petclinic.services;
 
 import com.echo.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 
 }
