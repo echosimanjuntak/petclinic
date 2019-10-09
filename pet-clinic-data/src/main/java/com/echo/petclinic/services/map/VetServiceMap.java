59abe81863de2b6c49cb20c93ee95c41b1a3f2df
echosimanjuntak <echo.w.m.simanjuntak@gvmnetworks.com>
2019-10-09 11:07:54 +0700

    Adding Spring Config

--- pet-clinic-data/src/main/java/com/echo/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/echo/petclinic/services/map/VetServiceMap.java
@@ -2,9 +2,11 @@ package com.echo.petclinic.services.map;
 
 import com.echo.petclinic.model.Vet;
 import com.echo.petclinic.services.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
--- pet-clinic-web/src/main/java/com/echo/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/echo/petclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import com.echo.petclinic.model.Owner;
 import com.echo.petclinic.model.Vet;
 import com.echo.petclinic.services.OwnerService;
 import com.echo.petclinic.services.VetService;
-import com.echo.petclinic.services.map.OwnerServiceMap;
-import com.echo.petclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,9 +13,9 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
