b22f9a644cf6c39c55621fc7f9a57dcd10c799df
echosimanjuntak <echo.w.m.simanjuntak@gvmnetworks.com>
2019-10-09 10:59:15 +0700

    Adding bootstrap for data.

--- pet-clinic-data/src/main/java/com/echo/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/echo/petclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package com.echo.petclinic.services.map;
 
 import com.echo.petclinic.model.Pet;
-import com.echo.petclinic.services.CrudService;
+import com.echo.petclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
--- pet-clinic-data/src/main/java/com/echo/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/echo/petclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package com.echo.petclinic.services.map;
 
 import com.echo.petclinic.model.Vet;
-import com.echo.petclinic.services.CrudService;
+import com.echo.petclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
